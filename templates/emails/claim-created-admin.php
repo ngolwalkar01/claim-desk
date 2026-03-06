2009bfc0227b6d3dda83ea8a0617086f5f458b3f
developervinit <vinit91.dev@gmail.com>
2026-03-06 18:21:33 +0530

    fix: Hook names invoked by a theme

--- templates/emails/claim-created-admin.php
+++ templates/emails/claim-created-admin.php
@@ -2,7 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-do_action( 'woocommerce_email_header', $email_heading, $email );
+do_action( 'woocommerce_email_header', $email_heading, $email ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- WooCommerce core hook.
 ?>
 <p><?php esc_html_e( 'A new claim has been created and is awaiting review.', 'claim-desk' ); ?></p>
 <?php wc_get_template( 'emails/claim-email-details.php', array( 'claim' => $claim ), 'claim-desk/', trailingslashit( CLAIM_DESK_PLUGIN_PATH ) . 'templates/' ); ?>
@@ -10,5 +10,4 @@ do_action( 'woocommerce_email_header', $email_heading, $email );
 if ( $additional_content ) {
 	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
 }
-do_action( 'woocommerce_email_footer', $email );
-
+do_action( 'woocommerce_email_footer', $email ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- WooCommerce core hook.
